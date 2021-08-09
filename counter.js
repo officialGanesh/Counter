a3d9a1d626461aeac72dc465a69e1dfc337248c6
Ganesh <ganuofficial2795@gmail.com>
2021-08-09 21:19:53 +0530

    JavaScript added

--- counter.js
+++ counter.js
@@ -1,3 +1,43 @@
 
 console.log("Counter 🚀")
 
+let count = 0
+let number = document.getElementById("num");
+let decrease = document.getElementById("decrease");
+let reset = document.getElementById("reset");
+let increase = document.getElementById("increase");
+
+let all_buttons = document.querySelectorAll('.btn');
+all_buttons.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        let change_num = e.currentTarget.id
+        
+        switch (change_num) {
+            case "decrease":
+                count--
+                number.textContent = count
+                number.style.color = "red"
+                
+                break;
+
+            case "reset":
+                
+                count = 0
+                number.textContent = count
+                number.style.color = "blue"
+                
+                break;
+
+            case "increase":
+                count++
+                number.textContent = count
+                number.style.color = "green"
+                
+                break;
+        
+            default:
+                count = 0
+                break;
+        }
+    })
+})
